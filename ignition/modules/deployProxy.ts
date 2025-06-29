d4cb8312bb60123c28ddf0c3873451230832ae2c
rajapolipilli <raja.p@xcubelabs.com>
2025-06-29 23:15:19 +0530

    .vscode remove

--- ignition/modules/deployProxy.ts
+++ ignition/modules/deployProxy.ts
@@ -5,7 +5,7 @@ const ADMIN_ADDRESS = "0x7fa859B1E10782Ae660154F9e6b62Df8E2561476"
 const ROYALTY_VAULT_ADDRESS = "0x7fa859B1E10782Ae660154F9e6b62Df8E2561476"
 
 
-const proxyModule = buildModule("ProxyModule", (m) => {
+const proxyModule = buildModule("ProxyModuleSonic", (m) => {
 
   const proxyAdminOwner ="0x53C8c0F72F879efD157189173E0530cE515BC290"// m.getAccount(0);
   //|| ADMIN_ADDRESS;
