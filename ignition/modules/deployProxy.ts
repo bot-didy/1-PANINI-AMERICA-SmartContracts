59ef3d4ce6e329ffd9924fca1ecc67c11c287dc7
rajapolipilli <raja.p@xcubelabs.com>
2025-07-10 15:02:16 +0530

    additions in deployProxy.ts

--- ignition/modules/deployProxy.ts
+++ ignition/modules/deployProxy.ts
@@ -1,21 +1,21 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 
-const ADMIN_ADDRESS = "0x7fa859B1E10782Ae660154F9e6b62Df8E2561476"
+const MANAGER_ADDRESS = "0x7fa859B1E10782Ae660154F9e6b62Df8E2561476"
 const ROYALTY_VAULT_ADDRESS = "0x7fa859B1E10782Ae660154F9e6b62Df8E2561476"
 
 
 const proxyModule = buildModule("ProxyModuleSonic", (m) => {
 
-  const proxyAdminOwner ="0x53C8c0F72F879efD157189173E0530cE515BC290"// m.getAccount(0);
-  //|| ADMIN_ADDRESS;
+  const proxyAdminOwner ="0x53C8c0F72F879efD157189173E0530cE515BC290";
 
   const implementation = m.contract("PaniniNFTs");
 
   const initializerData = m.encodeFunctionCall(implementation, "initialize", [
     proxyAdminOwner,//owner
     ROYALTY_VAULT_ADDRESS,// royalty receiver
-    5000
+    5000,
+    MANAGER_ADDRESS, // manager    
   ]);
 
   const proxy = m.contract("TransparentUpgradeableProxy", [
